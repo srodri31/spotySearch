afb951092a8b0ddb88bcde4e78aedc41352f33e4
Santiago Rodriguez <santi011c@outlook.com>
2020-01-15 14:56:57 -0500

    adding multiple words search

--- src/app/spotify.service.ts
+++ src/app/spotify.service.ts
@@ -21,6 +21,7 @@ export class SpotifyService {
       return of([]);
     }
 
+    name = name.replace(/ /g, '+');
     return this.http.get("https://api.spotify.com/v1/search?q="+name+"&type=artist", this.httpOptions)
       .pipe<Artist[]>(
         map( (result: { artists: { items: null }}) => result.artists.items)
